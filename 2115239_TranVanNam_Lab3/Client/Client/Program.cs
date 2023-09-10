19d316a3a21eeb897b8d45efb295e950f34ef810
Tran Van Nam <105662653+tvnam0605@users.noreply.github.com>
2023-09-10 18:27:45 +0700

    Lab 3

---
+++ 2115239_TranVanNam_Lab3/Client/Client/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+	class RetryUdpClient
+	{
+		private byte[] data;
+		private EndPoint Remote;
+
+		public RetryUdpClient()
+		{
+			string input, stringData;
+			int recv;
+			IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+			Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+			int sockopt = (int)server.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout);
+			Console.WriteLine("Gia tri timeout mac dinh: {0}", sockopt);
+
+			server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
+			sockopt = (int)server.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout);
+			Console.WriteLine("Gia tri timeout moi: {0}", sockopt);
+
+			string welcome = "Xin chao server";
+			data = Encoding.ASCII.GetBytes(welcome);
+			recv = SndRcvData(server, data, ipep);
+
+			if (recv > 0)
+			{
+				stringData = Encoding.ASCII.GetString(data, 0, recv);
+				Console.WriteLine(stringData);
+			}
+			else
+			{
+				Console.WriteLine("Khong the lien lac voi thiet bi o xa");
+				return;
+			}
+
+			// Khởi tạo biến Remote bên ngoài vòng lặp
+			Remote = new IPEndPoint(IPAddress.Any, 0);
+
+			while (true)
+			{
+				input = Console.ReadLine();
+
+				if (input == "exit")
+					break;
+
+				recv = SndRcvData(server, Encoding.ASCII.GetBytes(input), ipep);
+
+				if (recv > 0)
+				{
+					stringData = Encoding.ASCII.GetString(data, 0, recv);
+					Console.WriteLine(stringData);
+				}
+				else
+				{
+					Console.WriteLine("Khong nhan duoc cau tra loi");
+				}
+			}
+
+			Console.WriteLine("Dang dong client");
+			server.Close();
+		}
+		private int SndRcvData(Socket s, byte[] message, EndPoint rmtdevice)
+		{
+			int recv;
+			int retry = 0;
+
+			while (true)
+			{
+				Console.WriteLine("Truyen lai lan thu: #{0}", retry);
+
+				try
+				{
+					s.SendTo(message, message.Length, SocketFlags.None, rmtdevice);
+					data = new byte[1024];
+					recv = s.ReceiveFrom(data, ref rmtdevice); // Sử dụng rmtdevice đã truyền vào
+				}
+				catch (SocketException)
+				{
+					recv = 0;
+				}
+
+				if (recv > 0)
+				{
+					// Dữ liệu đã được nhận thành công
+					return recv;
+				}
+				else
+				{
+					// Gửi lại dữ liệu nếu không nhận được phản hồi và retry chưa vượt quá 4 lần
+					if (retry >= 4)
+					{
+						return 0;
+					}
+
+					retry++; // Tăng giá trị của retry
+				}
+			}
+		}
+
+
+
+		class Program
+		{
+			static void Main(string[] args)
+			{
+				RetryUdpClient client = new RetryUdpClient();
+			}
+		}
+	}
+}
+
+
+
+
---
+++ 2115239_TranVanNam_Lab3/Server/Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Tạo địa chỉ IP và cổng cho server
+			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+
+			// Tạo socket UDP
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+			// Bind socket với địa chỉ IP và cổng
+			serverSocket.Bind(serverEndPoint);
+
+			Console.WriteLine("Dang cho client ket noi den...");
+
+			// Tạo một luồng con để giả lập chậm trễ
+			Thread slowServerThread = new Thread(() =>
+			{
+				while (true)
+				{
+					// Ngừng máy chủ trong 5 giây để giả lập trường hợp máy chủ chậm trễ
+					Thread.Sleep(5000);
+				}
+			});
+
+			// Bắt đầu luồng giả lập chậm trễ
+			slowServerThread.Start();
+
+			while (true)
+			{
+				byte[] receiveBuffer = new byte[1024];
+				EndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+				// Nhận dữ liệu từ client
+				int bytesRead = serverSocket.ReceiveFrom(receiveBuffer, ref clientEndPoint);
+
+				// Chuyển dữ liệu nhận được thành chuỗi
+				string clientData = Encoding.ASCII.GetString(receiveBuffer, 0, bytesRead);
+				Console.WriteLine($"Thong diep duoc nhan tu {clientEndPoint}: {clientData}");
+
+				// Phản hồi client
+				string response = "Server da nhan duoc tin nhan cua ban: " + clientData;
+				byte[] sendBuffer = Encoding.ASCII.GetBytes(response);
+				serverSocket.SendTo(sendBuffer, clientEndPoint);
+			}
+		}
+	}
+}
