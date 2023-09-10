1842a72d8de5334914c404bc873f319290e6a9ff
Tran Van Nam <105662653+tvnam0605@users.noreply.github.com>
2023-09-10 17:05:45 +0700

    Add files via upload

---
+++ 2115239_TranVanNam_Lab2/Client/Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Dang ket noi voi server...");
+            try
+            {
+                serverSocket.Connect(serverEndPoint);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("Khong the ket noi den server");
+                Console.ReadKey();
+                return;
+            }
+            if (serverSocket.Connected)
+            {
+                byte[] buff = new byte[1024];
+                Console.WriteLine("Ket noi thanh cong voi server ...");
+                var byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                string str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine(str);
+            }
+
+            while (true)
+            {
+                byte[] buff = new byte[1024];
+                string str = Console.ReadLine();
+                buff = Encoding.ASCII.GetBytes(str);
+                serverSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+                var byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2115239_TranVanNam_Lab2/Server/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        private static int byteReceive;
+        private static string str;
+
+        static void Main(string[] args)
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+            ProtocolType.Tcp);
+            serverSocket.Bind(serverEndPoint);
+            serverSocket.Listen(10);
+            Socket clientSocket = serverSocket.Accept();
+            EndPoint clientEndPoint = clientSocket.RemoteEndPoint;
+            Console.WriteLine(clientEndPoint.ToString());
+            byte[] buff;
+            string hello = "Hello Client";
+            buff = Encoding.ASCII.GetBytes(hello);
+            clientSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+            while (true)
+            {
+                buff = new byte[1024];
+                byteReceive = clientSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine(str);
+                clientSocket.Send(buff, 0, byteReceive, SocketFlags.None);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
