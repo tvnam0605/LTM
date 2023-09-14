01be2221d5feb85b5eb713ba679392bd95033277
Tran Van Nam <105662653+tvnam0605@users.noreply.github.com>
2023-09-14 11:19:58 +0700

    upload file

---
+++ 2115239_TranVanNam_Lab4/ThongTinNhanVien_UDP/Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Share;
+namespace Client
+{
+	class Program
+	{
+		static void Main()
+		{
+			while (true)
+			{
+				Employee emp1 = new Employee();
+
+				Console.Write("Nhap EmployeeID: ");
+				emp1.EmployeeID = int.Parse(Console.ReadLine());
+
+				Console.Write("Nhap LastName: ");
+				emp1.LastName = Console.ReadLine();
+
+				Console.Write("Nhap FirstName: ");
+				emp1.FirstName = Console.ReadLine();
+
+				Console.Write("Nhap YearsService: ");
+				emp1.YearsService = int.Parse(Console.ReadLine());
+
+				Console.Write("Nhap Salary: ");
+				emp1.Salary = double.Parse(Console.ReadLine());
+
+				UdpClient udpClient = new UdpClient();
+				IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+
+				byte[] data = emp1.GetBytes();
+				udpClient.Send(data, data.Length, serverEndPoint);
+				Console.WriteLine("Data sent to server.");
+
+				udpClient.Close();
+
+				Console.Write("Ban co muon nhap tiep khong? (Khong de ket thuc, bat ky de nhap tiep): ");
+				string continueInput = Console.ReadLine();
+				if (continueInput.ToLower() == "khong")
+				{
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ 2115239_TranVanNam_Lab4/ThongTinNhanVien_UDP/Server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Share;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int port = 9050;
+			UdpClient udpServer = new UdpClient(port);
+			Console.WriteLine("Server dang nghe tren cong {0}...", port);
+
+			while (true)
+			{
+				IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, port);
+				byte[] data = udpServer.Receive(ref clientEndPoint);
+				Employee emp1 = new Employee(data);
+				Console.WriteLine("Du lieu nhan tu client:");
+				Console.WriteLine("emp1.EmployeeID = {0}", emp1.EmployeeID);
+				Console.WriteLine("emp1.LastName = {0}", emp1.LastName);
+				Console.WriteLine("emp1.FirstName = {0}", emp1.FirstName);
+				Console.WriteLine("emp1.YearsService = {0}", emp1.YearsService);
+				Console.WriteLine("emp1.Salary = {0}", emp1.Salary);
+
+				// Ghi dữ liệu vào tệp văn bản
+				WriteToTextFile(emp1);
+			}
+		}
+
+		// Phương thức để ghi dữ liệu vào tệp văn bản
+		static void WriteToTextFile(Employee employee)
+		{
+			try
+			{
+				string fileName = @"D:\LapTrinhMang\2115239_TranVanNam_Lab4\ThongTinNhanVien_UDP\Server\employee_data.txt"; // Đặt đường dẫn tệp văn bản tại đây
+				using (StreamWriter writer = new StreamWriter(fileName, true))
+				{
+					// Ghi dữ liệu vào tệp văn bản
+					writer.WriteLine("EmployeeID: " + employee.EmployeeID);
+					writer.WriteLine("LastName: " + employee.LastName);
+					writer.WriteLine("FirstName: " + employee.FirstName);
+					writer.WriteLine("YearsService: " + employee.YearsService);
+					writer.WriteLine("Salary: " + employee.Salary);
+					writer.WriteLine("------------------------------");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Lỗi khi ghi tệp văn bản: " + ex.Message);
+			}
+		}
+	}
+}
