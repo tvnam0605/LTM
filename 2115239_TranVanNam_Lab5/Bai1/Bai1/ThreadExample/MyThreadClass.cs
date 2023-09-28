1aea3a1f045281eb27755a6878a4c6dba345ee1c
Tran Van Nam <105662653+tvnam0605@users.noreply.github.com>
2023-09-28 18:05:27 +0700

    Bổ sung bài 1 lab 5

---
+++ 2115239_TranVanNam_Lab5/Bai1/Bai1/ThreadExample/MyThreadClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExample
+{
+    class MyThreadClass
+    {
+        private const int RANDOM_SLEEP_MAX = 1000;
+        private const int LOOP_COUNT = 10;
+
+        private String greeting;
+        public MyThreadClass(String greeting)
+        {
+            this.greeting = greeting;
+        }
+        public void runMyThread()
+        {
+            Random rand = new Random();
+            for (int x = 0; x < LOOP_COUNT; x++)
+            {
+                Console.WriteLine(greeting + "(Thread ID: " + Thread.CurrentThread.GetHashCode()+ ")");
+                try
+                {
+                    Thread.Sleep(rand.Next(0, RANDOM_SLEEP_MAX));
+                }
+                catch (ThreadInterruptedException)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ 2115239_TranVanNam_Lab5/Bai1/Bai1/ThreadExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThreadClass mtc1 = new MyThreadClass("Day la tieu trinh thu 1");
+            Thread thread1 = new Thread(new ThreadStart(mtc1.runMyThread));
+            thread1.Start();
+
+            MyThreadClass mtc2 = new MyThreadClass("Day la tieu trinh thu 2");
+            Thread thread2 = new Thread(new ThreadStart(mtc1.runMyThread));
+            thread2.Start();
+
+            Console.ReadKey();
+
+        }
+    }
+}
